d6cd04d0e606b27c08e3e4050bdaff1ddc8befc7
curder <curder@foxmail.com>
2019-04-28 12:03:52 +0800

    values

---
+++ values.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 字符串通过 + 拼接
+	fmt.Println("hello" + " world")
+
+	// 整型运算
+	fmt.Println("1 + 1 = " , 1 + 1)
+
+	// 浮点数运算
+	fmt.Println("7.0 / 3.0 = ", 7.0 / 3.0)
+
+	// 布尔型
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
