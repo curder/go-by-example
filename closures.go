4f2def6184e0f7132330770f3a863dc27983c47a
curder <curder@foxmail.com>
2019-04-29 14:05:59 +0800

    struct_methods

--- closures.go
+++ closures.go
@@ -5,7 +5,7 @@ package main
 import "fmt"
 
 // 这个 `intSeq` 函数返回另一个在 `intSeq` 函数体内定义的匿名函数。这个返回的函数使用闭包的方式 _隐藏_ 变量 `i`。
-func intSeq() func() int  {
+func intSeq() func() int {
 	i := 0
 	return func() int {
 		i += 1
@@ -13,7 +13,7 @@ func intSeq() func() int  {
 	}
 }
 
-func main()  {
+func main() {
 
 	// 我们调用 `intSeq` 函数，将返回值（也是一个函数）赋给`nextInt`。这个函数的值包含了自己的值 `i`，这样在每 次调用 `nextInt` 是都会更新 `i` 的值。
 	nextInt := intSeq()
