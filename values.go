57112a1b4eff6f045ed919140473e0ce5d7ef560
curder <curder@foxmail.com>
2019-04-30 18:05:48 +0800

    values

--- values.go
+++ values.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	// ➕ 当两边都是字符串时，结果为拼接；当两边都是整型时，结果为加法运算。
 	// 字符串通过 + 拼接
 	fmt.Println("hello" + " world")
 
-	// 整型运算
-	fmt.Println("1 + 1 = ", 1+1)
+	// 整型 + 运算
+	fmt.Println("1 + 1 =", 1+1)
 
 	// 浮点数运算
 	fmt.Println("7.0 / 3.0 = ", 7.0/3.0)
