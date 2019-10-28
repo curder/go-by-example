dc0d8d726b3b1e75fd175e11cd74c2cfa050da11
curder <curder@foxmail.com>
2019-10-28 18:52:11 +0800

    integer type

--- src/values/main.go
+++ src/values/main.go
@@ -10,6 +10,29 @@ func main() {
 	// 整型 + 运算
 	fmt.Println("1 + 1 =", 1+1)
 
+	// 十进制整形
+	var i1 = 101
+	fmt.Printf("%d\n", i1)
+	fmt.Printf("%b\n", i1) // 转换成二进制输出
+	fmt.Printf("%o\n", i1) // 转换成八进制输出
+	fmt.Printf("%x\n", i1) // 转换成十六进制输出
+
+	// 八进制整形
+	i2 := 077
+	fmt.Printf("%d\n", i2)
+
+	// 十六进制整形
+	i3 := 0x1234567
+	fmt.Printf("%d\n", i3)
+
+	// 查看变量类型
+	fmt.Printf("%T \n", i3)
+
+	// 声明 int8 类型变量
+	i4 := int8(32)
+	fmt.Printf("%T\n", i4)
+
+
 	// 浮点数运算
 	fmt.Println("7.0 / 3.0 = ", 7.0/3.0)
 
@@ -17,5 +40,4 @@ func main() {
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
-
 }
