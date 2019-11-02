987617374e1c010cdeadce1e275f70cb31bc236c
curder <curder@foxmail.com>
2019-11-02 21:40:21 +0800

    structure json

---
+++ src/structures/structure_json_test.go
@@ -0,0 +1,37 @@
+package structures
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+type JsonPerson struct {
+	Name string `json:"name"` // 反射键
+	Age int `json:"age"`
+}
+
+func TestStructureAndJson(t *testing.T)  {
+	p1:= JsonPerson{
+		Name: "curder",
+		Age: 28,
+	}
+
+	p2, error := json.Marshal(p1) // 序列化结构体
+ 	if error != nil {
+		t.Logf("mashal error %v", error)
+		return
+	}
+
+	t.Logf("\n序列化数据：%v\n", string(p2))
+
+	// 反序列化
+	var p3 JsonPerson
+	v1 := `{"name": "luo", "age": 28}`
+	error = json.Unmarshal([]byte(v1), &p3)
+	if(error != nil) {
+		t.Logf("unmashl error %v", error)
+		return
+	}
+	t.Logf("\n反序列化数据：%#v\n", p3)
+
+}
