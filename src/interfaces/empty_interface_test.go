8ce537ef9dbbb1d0da885e5e7b558dc9da95976d
curder <curder@foxmail.com>
2019-11-11 13:58:46 +0800

    empty interface

---
+++ src/interfaces/empty_interface_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+// 空接口
+// 是指没有定义任何方法的接口，因此任何类型都实现了空接口。
+// 空接口类型的变量可以存储任意类型的变量。
+// interface{} // 空接口
+func TestEmptyInterfaceForMap(t *testing.T) {
+	//var v1 = map[interface{}]interface{} // error: https://forum.golangbridge.org/t/practical-example-on-empty-interface/5268/3
+	v1 := make(map[string]interface{}, 10)
+	v1["name"] = "curder"
+	v1["age"] = 28
+	v1["hobby"] = "coding"
+	v1["married"] = false
+
+	t.Logf("\nv1的值为：%#v", v1)
+}
+
+// 空接口作为函数参数
+func show(a interface{}) {
+	fmt.Printf("类型: %T 值: %v\n", a, a)
+}
+
+func TestEmptyInterfaceForFunction(t *testing.T) {
+	show(false)
+	show(nil)
+
+	v2 := make(map[string]interface{}, 10)
+	show(v2)
+}
+
+// 类型断言
+// 空接口可以存储任意类型的值，那我们如何获取其存储的具体数据呢？
+
+func TestEmptyInterfaceTypeAssertion(t *testing.T) {
+	var x interface{}
+	x = "hello world!"
+	_, ok := x.(string)
+	if ok {
+		t.Log("\n传入的参数是字符串")
+	} else {
+		t.Log("\n类型断言失败!")
+	}
+
+	// 使用switch case判断类型
+	switch x.(type) {
+	case string:
+		t.Log("\n传入的参数是字符串", x)
+	case int:
+		t.Log("\n传入的参数是整型", x)
+	case bool:
+		t.Log("\n传入的参数是布尔值", x)
+	}
+}
