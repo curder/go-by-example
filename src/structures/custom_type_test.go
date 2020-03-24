97ff07a1715869a6dff74e3dd53df3f5635b3615
curder <curder@foxmail.com>
2020-03-24 15:26:28 +0800

    custom type add method

--- src/structures/custom_type_test.go
+++ src/structures/custom_type_test.go
@@ -10,7 +10,18 @@ import (
 // 自定义类型
 type myInt int
 
-func TestCustomType(t *testing.T)  {
+func TestCustomType(t *testing.T) {
 	var n1 myInt = 100
 	t.Logf("n1的值为：%v，类型为：%T \n", n1, n1)
+}
+
+func TestCustomTypeAddMethod(t *testing.T) {
+	var age = myInt(18)
+	age.incr()
+	t.Log(age)
+}
+
+// 给自己定义的类型添加一个方法
+func (m *myInt) incr() {
+	*m++
 }
