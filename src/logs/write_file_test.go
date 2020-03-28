673cacf78f4d3a3617fa8d4c6bb013a750810849
curder <curder@foxmail.com>
2020-03-28 16:10:50 +0800

    code style fixed

--- src/logs/write_file_test.go
+++ src/logs/write_file_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -14,14 +13,14 @@ func TestSetFileContentByIOWrite(t *testing.T) {
 	f, err := os.OpenFile(filePath, os.O_WRONLY|os.O_APPEND|os.O_CREATE|os.O_TRUNC, 0644)
 
 	if err != nil {
-		fmt.Println("文件发生错误")
+		t.Logf("文件发生错误, err: %v", err)
 		return
 	}
 
 	defer f.Close() // 延迟关闭文件
 
 	f.Write([]byte("hello\n"))
-	f.WriteString("World\n")
+	f.WriteString("world\n")
 }
 
 // 通过bufio的方式写文件
@@ -30,7 +29,7 @@ func TestSetFileContentByBufIO(t *testing.T) {
 	f, err := os.OpenFile(filePath, os.O_WRONLY|os.O_APPEND|os.O_CREATE|os.O_TRUNC, 0644)
 
 	if err != nil {
-		fmt.Println("文件发生错误")
+		t.Logf("文件发生错误，err: %v", err)
 		return
 	}
 
@@ -38,7 +37,7 @@ func TestSetFileContentByBufIO(t *testing.T) {
 
 	w := bufio.NewWriter(f)
 
-	w.WriteString("Hello World\n")
+	w.WriteString("hello world\n")
 	w.Flush() // 将缓存区的内容刷新到磁盘
 }
 
@@ -49,7 +48,7 @@ func TestSetFileContentByIOUtil(t *testing.T) {
 	err := ioutil.WriteFile(filePath, []byte(str), 0644)
 
 	if err != nil {
-		fmt.Printf("文件写入时发生错误")
+		t.Logf("文件写入时发生错误, err: %v", err)
 		return
 	}
 }
