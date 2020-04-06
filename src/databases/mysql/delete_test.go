8ba7f1f3997731c02aad03e9d95141f56fdedfb4
curder <curder@foxmail.com>
2020-04-06 16:15:09 +0800

    MySQL delete

---
+++ src/databases/mysql/delete_test.go
@@ -0,0 +1,22 @@
+package mysql
+
+import (
+	"fmt"
+	"testing"
+)
+
+// 删除行
+func TestDeleteRow(t *testing.T) {
+	sqlStr := "DELETE FROM users where id = ?"
+	ret, err := db.Exec(sqlStr, 3)
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success, affected rows:%d\n", n)
+}
