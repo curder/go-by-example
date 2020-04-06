bdc199c8cac5c569b70fa0b3b02259f8c3fe79f9
curder <curder@foxmail.com>
2020-04-06 16:47:56 +0800

    MySQL prepare

--- src/databases/mysql/read_test.go
+++ src/databases/mysql/read_test.go
@@ -58,3 +58,40 @@ func TestMultiRows(t *testing.T) {
 		fmt.Printf("id:%d name:%s age:%d\n", u.Id, u.Name, u.Age)
 	}
 }
+
+// 预处理查询
+// 优化MySQL服务器重复执行SQL的方法，可以提升服务器性能，提前让服务器编译，一次编译多次执行，节省后续编译的成本。
+// 避免SQL注入问题。
+func TestPrepareQueryRows(t *testing.T) {
+	sqlStr := "SELECT * FROM users WHERE id > ?"
+	stmt, err := db.Prepare(sqlStr)
+	if err != nil {
+		t.Logf("prepare failed, err:%v\n", err)
+		return
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query(0)
+	if err != nil {
+		t.Logf("query failed, err:%v\n", err)
+		return
+	}
+	defer rows.Close()
+
+	// 声明数据库结构体
+	type user struct {
+		Id   int    `json:"id"`
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}
+
+	// 循环读取结果集中的数据
+	for rows.Next() {
+		var u user
+		err := rows.Scan(&u.Id, &u.Name, &u.Age)
+		if err != nil {
+			fmt.Printf("scan failed, err:%v\n", err)
+			return
+		}
+		fmt.Printf("id:%d name:%s age:%d\n", u.Id, u.Name, u.Age)
+	}
+}
